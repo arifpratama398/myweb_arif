6db0c3551c1708a2bcb82c867c5932b0afab9053
Arif Naga Pratama <arifpratama398@gmail.com>
2019-11-24 16:24:52 +0700

    init commit web

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!-- saved from url=(0057)https://getbootstrap.com/docs/4.0/examples/sticky-footer/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Arif Naga Pratama - 185411183</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sticky-footer/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+
+  <body>
+
+    <!-- Begin page content -->
+    <main role="main" class="container">
+      <h1 class="mt-5">Arif Naga Pratama - 185411193</h1>
+      <p class="lead">Halaman ini dibuat untuk memenuhi praktikum Teknologi Cloud.</p>
+    </main>
+
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Teknik Informatika - STMIK Akakom Yogyakarta</span>
+      </div>
+    </footer>
+  
+
+</body></html>
